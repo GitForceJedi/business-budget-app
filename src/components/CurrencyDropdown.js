dc2ce87c2c419f98825e687134d81ee08fa5f764
GitForceJedi <Kencraw4ord@outlook.com>
2023-09-10 06:44:41 -0400

    changes applied from final project

---
+++ src/components/CurrencyDropdown.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const CurrencyDropdown = () => {
+  const { currency, dispatch } = useContext(AppContext);
+
+  const handleChange = (e) => {
+    const newCurrency = e.target.value;
+    dispatch({ type: 'CHG_CURRENCY', payload: newCurrency });
+  };
+
+  return (
+    <div>
+      <label htmlFor="currency-dropdown">Select Currency: </label>
+      <select id="currency-dropdown" onChange={handleChange} value={currency}>
+        <option value="$">$ Dollar</option>
+        <option value="£">£ Pound</option>
+        <option value="€">€ Euro</option>
+        <option value="₹">₹ Rupee</option>
+      </select>
+    </div>
+  );
+};
+
+export default CurrencyDropdown;
